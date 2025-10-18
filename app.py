4d31319fbb3b71369db1f1f2bcb39cffc7d67000
shania-codes <sh@ni.a>
2025-10-18 18:19:48 +0100

    Live updates when new messages are sent

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.secret_key = "worst_admin"
 socketio = SocketIO()
 socketio.init_app(app)
 
+
 def get_db():
     db = sqlite3.connect("data.db")
     return db
@@ -25,9 +26,15 @@ def create_db():
 create_db()
 
 
+# Routes
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html", messages=get_last_n_messages())
+
+# Error Handling
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect(url_for('index'))
 
 
 # SocketIO
@@ -54,12 +61,11 @@ def new_message(json):
         return # Stops the rest of the function from running
 
     # boolean, AI(LLM or other) marks message as acceptable (1) or unacceptable (0)
-    accepted = 1 if "badword" not in original.lower() else 0 
-    # TODO AI(LLM or other) integration
+    accepted = ai_message_check(original)
  
     # safe, LLM rewritten version of accepted messages
     if accepted == 1:
-        safe = original #TODO LLM integration
+        safe = ai_safe_rewrite(original)
     else:
         safe = None # Just store the original message the AI(LLM or other) marked it for deletion so it didn't rewrite a safe version
 
@@ -68,18 +74,31 @@ def new_message(json):
     cursor = db.cursor()
     
     cursor.execute("INSERT INTO messages (ip, timestamp, original, accepted, safe) VALUES (?, ?, ?, ?, ?)", (ip, timestamp, original, accepted, safe))
+
+    emit("new_message_broadcast", {"id":cursor.lastrowid, "ip":ip, "timestamp":timestamp, "original":original, "accepted":accepted, "safe":safe}, broadcast=True)
     
     db.commit()
     db.close()
+
+    
     
 ## Load older
 # TODO when user scrolls back far enough return more older messages if any, if less than n messages (including 0 older messages) display a message so the user knows they have seen loaded all messages
 
 
-# Error Handling
-@app.errorhandler(404)
-def page_not_found(error):
-    return redirect(url_for('index'))
+# Python Functions
+## TODO AI(LLM or other) integration
+def ai_message_check(original):
+    # Fake implementation for testing since API is down
+    if "kill" in original.lower():
+        return 0 # Message is marked as UNSAFE by AI
+    else:
+        return 1 # Message is marked as safe by AI
+
+def ai_safe_rewrite(original):
+    # TODO LLM integration
+    # Not implemented as API is down
+    return original # Temp
 
 # Database functions
 ## Get Last n messages TODO untested
